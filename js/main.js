c60092a098c3c3ac34ba99096a6c0c5d9fe9c468
Tam Denholm <tam@redraptor.co.uk>
2019-02-09 18:44:02 +0000

    trying to figure out JS errors

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-var main = {
-
-    game_tick: 1000,
-
-    // main loop of the game
-    run: function(){
-        // initial load
-        resources.update();
-        actions.attach();
-
-        // begin loop
-        setInterval(function(){
-            resources.energy--;
-            resources.update();
-
-        }, this.game_tick);
-    },
-
-    die: function(){
-        $('body').html('<p class="container mt-5">You died sucka!<br><button onclick="location.reload();">Try again</button></p>');
-    },
-
-
-};
-
 var resources = {
 
     energy: 10,
@@ -39,17 +14,22 @@ var resources = {
         }
 
         // update resource UI
-        $('#energy').text(resources.energy);
-        $('#wood').text(resources.wood);
-        $('#stone').text(resources.stone);
-        $('#metal').text(resources.metal);
-        $('#silicon').text(resources.silicon);
+        $('#energy').text(this.energy);
+        $('#wood').text(this.wood);
+        $('#stone').text(this.stone);
+        $('#metal').text(this.metal);
+        $('#silicon').text(this.silicon);
     },
 
-    add_energy: function(i){
+    energy: function(i){
         this.energy += i;
         this.update();
-    }
+    },
+
+    wood: function(i){
+        this.wood += i;
+        this.update();
+    },
 };
 
 var actions = {
@@ -57,9 +37,42 @@ var actions = {
     // attach the actions to the DOM
     attach: function(){
         // add energy button
-        $('#add_energy').on('click', function(){
-            resources.add_energy(1);
+        $('#gather_wood').on('click', function(){
+            resources.energy(-1);
+            resources.wood(this.random(1,2));
         });
+    },
+
+    random: function(min, max){
+        return Math.floor((Math.random() * max) + min);
     }
 
-}
+};
+
+var main = {
+
+    game_tick: 1000,
+
+    // main loop of the game
+    run: function(){
+        // initial load
+        resources.update();
+        actions.attach();
+
+        // begin loop
+        /*
+        setInterval(function(){
+            // only lose energy at night, staying warm
+            //resources.energy--;
+            resources.update();
+
+        }, this.game_tick);
+        */
+    },
+
+    die: function(){
+        $('body').html('<p class="container mt-5">You died sucka!<br><button onclick="location.reload();">Try again</button></p>');
+    },
+
+
+};
