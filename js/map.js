f617725d8ef5e7825b7ce35d9bb78615778f2d8d
Tam Denholm <tam@redraptor.co.uk>
2019-02-12 19:09:27 +0000

    refactored the map object some more player now stands over the resource

--- js/map.js
+++ js/map.js
@@ -3,52 +3,60 @@ let map = {
     cols: 9,
     rows: 9,
     tile_size: 64,
+    spritesheet: null,
+    context: null,
+    resources: {
+        'stone': [[1,3],[4,4],[8,1]],
+        'wood': [[5,2],[6,6],[2,5]]
+    },
 
     // initialisation
     init: function(){
+        this.context = document.getElementById('map').getContext('2d');
+        // fix this load image thing
+        this.spritesheet = document.getElementById('spritesheet');
+
+        // draw the map
         this.draw_map();
     },
 
     draw_map: function(){
-        let context = document.getElementById('map').getContext('2d');
-        // figure out a better way to load images
-        let spritesheet = document.getElementById('spritesheet');
-
         // place the sprites onto the canvas
-        for(let c = 0; c < this.cols; c++){
-            for(let r = 0; r < this.rows; r++){
-                // place ground
-                context.drawImage(
-                    spritesheet,            // spritesheet image for map
-                    (this.get_sprite('grass') - 1) * this.tile_size, // spritesheet start x
-                    0,                      // spritesheet start y
-                    this.tile_size,              // how much of spritesheet to move x
-                    this.tile_size,              // how much of spritesheet to move x
-                    c * this.tile_size,          // where to place on canvas x
-                    r * this.tile_size,          // where to place on cavas y
-                    this.tile_size,              // how much of canvas to draw x
-                    this.tile_size               // how much of canvas to draw y
-                );
-                // place objects over ground
-                if(this.get_tile((c + 1), (r + 1))){
-                    context.drawImage(
-                        spritesheet,            // spritesheet image for map
-                        (this.get_tile((c + 1),(r + 1)) - 1) * this.tile_size, // spritesheet start x
-                        0,                      // spritesheet start y
-                        this.tile_size,              // how much of spritesheet to move x
-                        this.tile_size,              // how much of spritesheet to move x
-                        c * this.tile_size,          // where to place on canvas x
-                        r * this.tile_size,          // where to place on cavas y
-                        this.tile_size,              // how much of canvas to draw x
-                        this.tile_size               // how much of canvas to draw y
-                    );
-                }
+        for(let x = 0; x < this.cols; x++){
+            for(let y = 0; y < this.rows; y++){
+                // default grass everywhere
+                this.draw_tile('grass', x, y);
             }
         }
+        // after we've drawn the map
+        // place the resources
+        $.each(this.resources, function(resource,positions){
+            $.each(positions, function(k, position){
+                map.draw_tile(resource, position[0]-1, position[1]-1);    
+            })
+        });
+        // get the player position
+        let human = player.get_position();
+        this.draw_tile('player', human[0]-1, human[1]-1);
     },
 
-    get_sprite: function(name, level = 0){
+    // tell this what to place, and where to place it on the map
+    draw_tile: function(sprite, x, y){
+        this.context.drawImage(
+            this.spritesheet,            // spritesheet image for map
+            (this.get_sprite(sprite) - 1) * this.tile_size, // spritesheet start x
+            0,                      // spritesheet start y
+            this.tile_size,              // how much of spritesheet to move x
+            this.tile_size,              // how much of spritesheet to move x
+            x * this.tile_size,          // where to place on canvas x
+            y * this.tile_size,          // where to place on cavas y
+            this.tile_size,              // how much of canvas to draw x
+            this.tile_size               // how much of canvas to draw y
+        );
+    },
 
+    // lookup function that makes it easier to specify a sprite
+    get_sprite: function(name, level = 3){
         if(name == 'stone'){
             switch (level) {
                 case 1: return 1;
@@ -58,7 +66,7 @@ let map = {
                 default: return 1;
             }
         }
-        if(name == 'tree'){
+        if(name == 'wood'){
             switch (level) {
                 case 1: return 5;
                 case 2: return 6;
@@ -76,6 +84,7 @@ let map = {
         console.log('Specified a sprite that doesnt exist!');
         return false;
     },
+
 
     get_tile: function(x,y){
         // get the player position
